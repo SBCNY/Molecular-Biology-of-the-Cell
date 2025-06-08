aedbef584f3a3188e33aa4a0d1ce5382fa1a4a49
JensHansen1 <34044014+JensHansen1@users.noreply.github.com>
2025-06-08 19:29:05 -0400

    Update MBC_pathNet_launch_within_R.R

--- MBCO_windows_application/MBC_pathNet_launch_within_R.R
+++ MBCO_windows_application/MBC_pathNet_launch_within_R.R
@@ -105,7 +105,7 @@ is_linux = !is_windows
     indexUseCustomizedColors = grep("Bardiagram_options_class\tCustomized_colors",mbcPathNet_parameter_lines)
     mbcPathNet_parameter_lines[indexUseCustomizedColors] = "Bardiagram_options_class\tCustomized_colors\tTrue"
     writeLines(mbcPathNet_parameter_lines,complete_output_parameterSettings_fileName)
-    exe_path <- file.path(mbc_pathNet_directory, "MBC_PathNet.exe")
+    exe_path = file.path(mbc_pathNet_directory, "MBC_PathNet.exe")
     if (is_windows)
     {#Begin
         cmd = paste0('"', exe_path, '" --input-dir ', '"', degs_directory, '"', ' --custom-1-column-names')
